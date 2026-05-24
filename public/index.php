abc524dd2b3dfa048b12d6af8bd5f1db93f9c70d
patrickloffler <patrick.loffler@tul.cz>
2026-05-24 23:54:17 +0200

    test

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+/* lmao */
 $baseDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
 define('BASE_URL', $baseDir);
 
